bb89c6e4ef5ddcc1d4f61858028b5227857849ef
Jarosław Domański <domanoid@gmail.com>
2019-09-26 22:13:16 +0200

    bite 9 done

--- 9/palindrome.py
+++ 9/palindrome.py
@@ -4,7 +4,7 @@ import os
 import urllib.request
 
 DICTIONARY = os.path.join('/tmp', 'dictionary_m_words.txt')
-urllib.request.urlretrieve('http://bit.ly/2Cbj6zn', DICTIONARY)
+# urllib.request.urlretrieve('http://bit.ly/2Cbj6zn', DICTIONARY)
 
 
 def load_dictionary():
@@ -12,17 +12,25 @@ def load_dictionary():
     with open(DICTIONARY) as f:
         return (word.lower().strip() for word in f.readlines())
 
+# additional imports
+import re
 
-def is_palindrome(word):
+def is_palindrome(word: str):
     """Return if word is palindrome, 'madam' would be one.
        Case insensitive, so Madam is valid too.
        It should work for phrases too so strip all but alphanumeric chars.
        So "No 'x' in 'Nixon'" should pass (see tests for more)"""
-    pass
+    word = word.lower()
+    pattern = re.compile(r'[\W_]+', re.UNICODE)
+    word = pattern.sub('', word)
+    return True if word == word[::-1] else None
 
 
-def get_longest_palindrome(words=None):
+def get_longest_palindrome(words: list=None):
     """Given a list of words return the longest palindrome
        If called without argument use the load_dictionary helper
        to populate the words list"""
-    pass
+    if not words:
+        words = load_dictionary()
+    words = list(filter(lambda word: is_palindrome(word), words))
+    return max(words, key=lambda word: len(word))
